b35a8ee6c9ae8ce7f25eb6986052968f71fe21ad
Luca Nicolas Terfehr <luca.terfehr@outlook.de>
2026-06-26 15:55:47 +0200

    Updated the SwinNet_test.py and added the requirements.txt

--- SwinNet_test.py
+++ SwinNet_test.py
@@ -6,6 +6,8 @@
 @time: 2021/5/27 09:34
 """
 
+import time
+
 import torch
 import torch.nn.functional as F
 import sys
@@ -19,10 +21,12 @@ from data import test_dataset
 parser = argparse.ArgumentParser()
 parser.add_argument('--testsize', type=int, default=384, help='testing size')
 parser.add_argument('--gpu_id', type=str, default='0', help='select gpu id')
-parser.add_argument('--test_path',type=str,default='./RGBD_for_test/',help='test dataset path')
-opt = parser.parse_args()
+parser.add_argument('--rgb', type=str, help="path to rgb images")
+parser.add_argument('--depth', type=str, help="path to depth images")
+parser.add_argument('--gt', type=str, help="path to gt images")
+parser.add_argument('--target', type=str, help="path to save results")
 
-dataset_path = opt.test_path
+opt = parser.parse_args()
 
 #set device for test
 if opt.gpu_id=='0':
@@ -37,70 +41,42 @@ model = SwinNet()
 model.load_state_dict(torch.load('./cpts/SwinTransNet_epoch_best.pth'))
 model.cuda()
 model.eval()
-#
-# #test
-# # test_datasets = ['CSSD','ECSSD','HKU-IS','PASCAL-S','DUT-OMROM','SOD','DUTS']
-# test_datasets = ['VT821']
-# for dataset in test_datasets:
-#     sal_save_path = './test_maps//SwinNet_RGBT/' + dataset + '/'
-#     edge_save_path = './test_maps//SwinNet_RGBT/Edge/' + dataset + '/'
-#     if not os.path.exists(sal_save_path):
-#         os.makedirs(sal_save_path)
-#         os.makedirs(edge_save_path)
-#     image_root = dataset_path + dataset + '/RGB/'
-#     gt_root = dataset_path + dataset + '/GT/'
-#     depth_root = dataset_path + dataset + '/T/'
-#     test_loader = test_dataset(image_root, gt_root, depth_root, opt.testsize)
-#     for i in range(test_loader.size):
-#         image, gt, depth, name, image_for_post = test_loader.load_data()
-#         gt = np.asarray(gt, np.float32)
-#         gt /= (gt.max() + 1e-8)
-#         image = image.cuda()
-#         depth = depth = depth.repeat(1,3,1,1).cuda()
-#         # print(depth.shape)
-#         res,edge = model(image,depth)
-#         res = F.upsample(res, size=gt.shape, mode='bilinear', align_corners=False)
-#         edge = F.upsample(edge, size=gt.shape, mode='bilinear', align_corners=False)
-#         res = res.sigmoid().data.cpu().numpy().squeeze()
-#         edge = edge.sigmoid().data.cpu().numpy().squeeze()
-#         res = (res - res.min()) / (res.max() - res.min() + 1e-8)
-#         edge = (edge - edge.min()) / (edge.max() - edge.min() + 1e-8)
-#         print('save img to: ', sal_save_path + name)
-#         cv2.imwrite(sal_save_path + name, res * 255)
-#         cv2.imwrite(edge_save_path + name, edge * 255)
-#     print('Test Done!')
 
-# model.cuda()
-# model.eval()
+image_root = opt.rgb if opt.rgb.endswith('/') else opt.rgb + '/'
+gt_root = opt.gt if opt.gt.endswith('/') else opt.gt + '/'
+depth_root = opt.depth if opt.depth.endswith('/') else opt.depth + '/'
+target_path = opt.target if opt.target.endswith('/') else opt.target + '/'
+eval = target_path + 'evaluation.txt'
 
-# test
+test_loader = test_dataset(image_root, gt_root, depth_root, opt.testsize)
 
-test_datasets = ['SIP','SSD','RedWeb','NJU2K','NLPR','STERE','DES','LFSD']
-# test_datasets = ['nju2k','stere','sip']
-for dataset in test_datasets:
-    save_path = './test_maps/SwinNet/' + dataset + '/'
-    edge_save_path = './test_maps/SwinNet/' + dataset + '/'
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
-        os.makedirs(edge_save_path)
-    image_root = dataset_path + dataset + '/RGB/'
-    gt_root = dataset_path + dataset + '/GT/'
-    depth_root = dataset_path + dataset + '/depth/'
-    test_loader = test_dataset(image_root, gt_root, depth_root, opt.testsize)
-    for i in range(test_loader.size):
-        image, gt, depth, name, image_for_post = test_loader.load_data()
-        gt = np.asarray(gt, np.float32)
-        gt /= (gt.max() + 1e-8)
-        image = image.cuda()
-        depth = depth = depth.repeat(1,3,1,1).cuda()
-        res, edge = model(image,depth)
-        res = F.upsample(res, size=gt.shape, mode='bilinear', align_corners=False)
-        edge = F.upsample(edge, size=gt.shape, mode='bilinear', align_corners=False)
-        res = res.sigmoid().data.cpu().numpy().squeeze()
-        edge = edge.sigmoid().data.cpu().numpy().squeeze()
-        res = (res - res.min()) / (res.max() - res.min() + 1e-8)
-        edge = (edge - edge.min()) / (edge.max() - edge.min() + 1e-8)
-        print('save img to: ',save_path+name)
-        cv2.imwrite(save_path + name, res*255)
-        cv2.imwrite(edge_save_path + name, edge * 255)
-    print('Test Done!')
+torch.cuda.synchronize()
+start_time = time.time()
+
+for i in range(test_loader.size):
+    print(f'Processing image {i+1}/{test_loader.size}')
+    image, gt, depth, name, image_for_post = test_loader.load_data()
+    gt = np.asarray(gt, np.float32)
+    gt /= (gt.max() + 1e-8)
+    image = image.cuda()
+    depth = depth.repeat(1,3,1,1).cuda()
+
+    res, edge = model(image,depth)
+    res = F.interpolate(res, size=gt.shape[:2], mode='bilinear', align_corners=False)
+    res = res.sigmoid().data.cpu().numpy().squeeze()
+    res = (res - res.min()) / (res.max() - res.min() + 1e-8)
+
+
+    cv2.imwrite(target_path + name, (res*255).astype(np.uint8))
+
+end_time = time.time()
+total_time = end_time - start_time
+images_processed = test_loader.size
+average_time_per_image = total_time / images_processed if images_processed > 0 else 0
+
+with open(eval, 'w') as f:
+    f.write(f'Total Time: {total_time:.4f} seconds\n')
+    f.write(f'Images Processed: {images_processed}\n')
+    f.write(f'Average Time Per Image: {average_time_per_image:.4f} seconds\n')
+
+print('Test Done!')
